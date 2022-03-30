16cc9605b12da27b59dcc296fd339dce06573c24
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-03-29 22:52:24 -0700

    'commit'

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 const fs = require('fs');
-
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-
 const generatePage = require('./src/page-template.js')
 
-fs.writeFile('index.html', generatePage(name, github), err => {
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github)
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('index.html', pageHTML, err => {
   if (err) throw err;
 
   console.log("Portfolio complete! Check out index.html to see the output!");
 })
-
-
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i++) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('===============');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs)
