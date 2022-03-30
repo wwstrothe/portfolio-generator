4128fc50ce77ebc4f3ac1f8e194ae88fae523eae
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-03-29 22:28:41 -0700

    created html and modularized code

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-  console.log('===============');
+const generatePage = (name, github) => {
+  return`
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
 };
 
-printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+})
+
+
+
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('===============');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs)
