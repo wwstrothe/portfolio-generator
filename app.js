56f706cc1a54c75a84743220c589576b27456272
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-04-07 15:23:20 -0700

    modified the HTML template to handle more data

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js')
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// })
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -160,5 +153,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("index.html", pageHTML, (err) => {
+      if (err) throw err;
+
+      console.log(
+        "Portfolio complete! Check out index.html to see the output!"
+      );
+    });
   })
